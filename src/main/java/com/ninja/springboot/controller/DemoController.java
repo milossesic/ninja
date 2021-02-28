7ddd1c358c55948b1172cda2a5c35dc014c31d74
milossesic <milosh.sesic@gmail.com>
2021-02-28 21:51:12 +0100

    initial commit

---
+++ src/main/java/com/ninja/springboot/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.ninja.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class DemoController {
+	
+	@GetMapping("/hello")
+	public String sayHello(Model theModel) {
+		theModel.addAttribute("theDate", new java.util.Date());
+		return "helloworld";
+	}
+	
+	
+
+}
